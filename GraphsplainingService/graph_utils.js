ef13289e673c6f64db2231da69568323d4ef4d7a
Jacob McCrumb <jsmccrumb@gmail.com>
2019-09-14 10:59:56 -0400

    Add runExplainAsync to graphUtils and tests, resolves #1

--- GraphsplainingService/graph_utils.js
+++ GraphsplainingService/graph_utils.js
@@ -1,4 +1,58 @@
 const crypto = require('crypto');
+const neo4j = require('neo4j-driver').v1;
+
+const explainDriver = neo4j.driver(process.env.NEO4J_EXPLAIN_BOLT,
+    neo4j.auth.basic(process.env.NEO4J_EXPLAIN_USER,
+        process.env.NEO4J_EXPLAIN_PASS));
+const mainDriver = neo4j.driver(process.env.NEO4J_MAIN_BOLT,
+    neo4j.auth.basic(process.env.NEO4J_MAIN_USER, process.env.NEO4J_MAIN_PASS));
+
+/**
+ * runs a read transaction using given session, query, and optional params
+ * @return {neo4j.statementResult} statementResult
+ */
+const readTransactionAsync = async (session, query, params) => {
+  return await session.readTransaction(async (tx) => {
+    return await tx.run(query, params);
+  });
+};
+
+/**
+ * Checks that the given driver can reach the graph
+ *
+ * @param {neo4j.driver} driver
+ * @return {boolean} isReady Whether connection is live
+ */
+const testConnectionAsync = async (driver) => {
+  const session = driver.session();
+  try {
+    const checkUser = 'call dbms.showCurrentUser()';
+    const resp = await readTransactionAsync(session, checkUser);
+    session.close();
+    // it does not matter who user it,
+    // just checking that connection to db is live for current user
+    return !!resp.records[0];
+  } catch {
+    session.close();
+    return false;
+  }
+};
+
+/**
+ * checks whether connections to both explain and main graphs are ready
+ * @return {Object} connections
+ * @return {boolean} connections.main Whether connection to main graph is ready
+ * @return {boolean} connections.explain
+ *     Whether connection to explain graph is ready
+ */
+const testConnectionsAsync = async () => {
+  const connections = {
+    main: (await testConnectionAsync(mainDriver)),
+    explain: (await testConnectionAsync(explainDriver)),
+  };
+  return connections;
+};
+
 /**
  * Removes comments from a cypher query
  */
@@ -20,8 +74,35 @@ const queryId = (query) => {
   return crypto.createHash('sha1').update(strippedQuery).digest('base64');
 };
 
+/**
+ * Makes query into an EXPLAIN query by removing comments
+ * and any number of initial explains or profiles
+ * @param {string} query
+ * @return {string} formattedQuery
+ */
+const transformQueryToExplain = (query) => {
+  query = removeComments(query);
+  // REGEX ^ = start of string
+  // (\s*(explain|profile)) = 0+ whitespace characters then explain or profile
+  // * = 0+ of that last group
+  // i = case insensitive
+  query = query.replace(/^(\s*(explain|profile))*/i, 'EXPLAIN ');
+  return query;
+};
+
+/**
+ * Runs explain against main graph for given query
+ * @param {string} query Query that needs explaining
+ * @return {neo4j.statementResult} result of explain
+ */
+const runExplainAsync = async (query) => {
+  query = transformQueryToExplain(query);
+  return await readTransactionAsync(mainDriver.session(), query);
+};
 
 module.exports = {
   removeComments,
   queryId,
+  testConnectionsAsync,
+  runExplainAsync,
 };
--- GraphsplainingService/graph_utils.test.js
+++ GraphsplainingService/graph_utils.test.js
@@ -25,3 +25,32 @@ test('queryId returns same id no matter whitespace or comments or case', () => {
   // i.e. expect all the ids to be the same
   expect(ids.every(id => id === ids[0])).toBeTruthy();
 });
+
+test('graphUtils can connect to graph', async () => {
+  const connections = await graphUtils.testConnectionsAsync();
+  expect(connections.main).toBeTruthy();
+  expect(connections.explain).toBeTruthy();
+});
+
+test('runExplainAsync returns explain plan for queries', async () => {
+  // include a parameter in the return so it will fail to run if not explain
+  const testQueries = [
+    `EXPLAIN PROFILE MATCH (n) RETURN n, $foo`,
+    `PROFILE MATCH (n) RETURN n, $foo`,
+    `// Some comment
+    PROFILE MATCH (n) RETURN n, $foo`,
+    `// Some Comment
+    profile profile match (n) return n, $foo`,
+    `MATCH (n) return n, $foo`,
+    `//Comment
+    MATCH (n) RETURN n, $foo`,
+    `// one last
+    PROFILE
+    PROfile MATCH (n) RETURN n, $foo`,
+  ];
+  await testQueries.forEach(async (q) => {
+    const result = await graphUtils.runExplainAsync(q);
+    expect(result.statement.plan).toBeTruthy();
+  });
+});
+
